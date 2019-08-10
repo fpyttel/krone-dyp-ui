11a477d0d42b482b2a48067357fac5baa1d16afc
Ferdinand Pyttel <ferdinand@pyttel.de>
2019-08-10 15:24:23 +0200

    add copyright and versions to footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,19 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
+  uiVersion;
+  apiVersion;
+
   constructor(translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
      // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use(translate.getBrowserLang() ? translate.getBrowserLang() : 'en');
+
+    // get version info
+    this.uiVersion = version;
+    this.apiVersion = '1.2.0';
   }
 
 }
